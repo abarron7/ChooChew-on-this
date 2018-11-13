f38edfc2f5b8d42fd38fb0b6f1c9bb165a1d63da
Alex Barron <abarron4544@yahoo.com>
2018-11-12 18:37:08 -0700

    initial setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  var config = {
+    apiKey: "AIzaSyDkaLB3t0YwoZ9oGZXU6wU5EAxPGw-JO1s",
+    authDomain: "trainscheduler-5f27a.firebaseapp.com",
+    databaseURL: "https://trainscheduler-5f27a.firebaseio.com",
+    projectId: "trainscheduler-5f27a",
+    storageBucket: "",
+    messagingSenderId: "237555827812"
+  };
+  firebase.initializeApp(config);
+
